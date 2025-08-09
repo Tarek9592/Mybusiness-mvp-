13f7d1a85154a7f4cefbb939038cf7cdb8975fc9
Tarek9592 <a4work95@gmail.com>
2025-08-09 18:56:44 +0300

    Update page.tsx

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,2 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
-export default function Page(){ return <div className="container py-12"><SignIn routing="path" path="/sign-in" /></div>; }
+
+export default function Page() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", padding: "50px" }}>
+      <SignIn />
+    </div>
+  );
+}
